c604af36574476ff7c1d81da98f79e4782a39659
Chuck Konkol <ckonkol@gmail.com>
2020-03-20 12:51:28 -0500

    Add files via upload

---
+++ precache-manifest.bb8b97ef79691d75a556d5a1448cb866.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c413973cc73be49fb24e",
+    "url": "/static/css/main.d0f40cf8.chunk.css"
+  },
+  {
+    "revision": "c413973cc73be49fb24e",
+    "url": "/static/js/main.eadd6972.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "69a5e3b821c0a0fc54b4",
+    "url": "/static/js/2.5dac9a04.chunk.js"
+  },
+  {
+    "revision": "6d48060669b7bc1354f7808136c66877",
+    "url": "/index.html"
+  }
+];
